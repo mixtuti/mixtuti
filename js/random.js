9f462eda3909bc6bdd3dbce373ef7a3a79201638
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-04-19 12:00:39 +0900

    Add files via upload

--- js/random.js
+++ js/random.js
@@ -1,7 +1,7 @@
 const articles = [
     {
         image: "https://mixtuti.github.io/mixtuti/img/favcon.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/bot.html'>Discord Bot</a>",
+        title: "<a href='../portfolio/bot.html'>Discord Bot</a>",
         description: "This is the description of article 1."
     },
     {
@@ -11,82 +11,82 @@ const articles = [
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0015.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/ose.html'>逢瀬</a>",
+        title: "<a href='../portfolio/ose.html'>逢瀬</a>",
         description: "This is the description of article 3."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0038.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/providing.html#tower'>鬼形獣タワー</a>",
+        title: "<a href='../portfolio/providing.html#tower'>鬼形獣タワー</a>",
         description: "This is the description of article 4."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0014.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/akinasi.html'>あきなし & あきなし2</a>",
+        title: "<a href='../portfolio/akinasi.html'>あきなし & あきなし2</a>",
         description: "This is the description of article 5."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0016.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/enemy.html'>レグ・ダーティ</a>",
+        title: "<a href='../portfolio/enemy.html'>レグ・ダーティ</a>",
         description: "This is the description of article 6."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0012.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/pon.html'>Pon DE Touhou</a>",
+        title: "<a href='../portfolio/pon.html'>Pon DE Touhou</a>",
         description: "This is the description of article 7."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0017.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/hunter.html'>ハンターアビリティ</a>",
+        title: "<a href='../portfolio/hunter.html'>ハンターアビリティ</a>",
         description: "This is the description of article 8."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0013.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/kemofan.html'>ケモノファンタジー</a>",
+        title: "<a href='../portfolio/kemofan.html'>ケモノファンタジー</a>",
         description: "This is the description of article 9."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0018.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/octo.html'>Octogether</a>",
+        title: "<a href='../portfolio/octo.html'>Octogether</a>",
         description: "This is the description of article 10."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0019.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/pc.html'>Perfect Crime～めざせ完全犯罪～</a>",
+        title: "<a href='../portfolio/pc.html'>Perfect Crime～めざせ完全犯罪～</a>",
         description: "This is the description of article 11."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0027.jpeg",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/klonoa.html'>世界が望んだ贈りもの ～風のクロノア２ 20周年記念非公式イラストアンソロジー～</a>",
+        title: "<a href='../portfolio/klonoa.html'>世界が望んだ贈りもの ～風のクロノア２ 20周年記念非公式イラストアンソロジー～</a>",
         description: "This is the description of article 12."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/favcon.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/event.html#autumn'>秋の企画ジャム2021</a>",
+        title: "<a href='../portfolio/event.html#autumn'>秋の企画ジャム2021</a>",
         description: "This is the description of article 13."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0164.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/mochicat.html'>モチネコ</a>",
+        title: "<a href='../portfolio/mochicat.html'>モチネコ</a>",
         description: "This is the description of article 14."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0146.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/as.html'>アクションシューター</a>",
+        title: "<a href='../portfolio/as.html'>アクションシューター</a>",
         description: "This is the description of article 15."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0133.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/japansige.html'>攻城戦VR</a>",
+        title: "<a href='../portfolio/japansige.html'>攻城戦VR</a>",
         description: "This is the description of article 16."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/favcon.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/event.html#spring'>2022春ジャム</a>",
+        title: "<a href='../portfolio/event.html#spring'>2022春ジャム</a>",
         description: "This is the description of article 17."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0025.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/denki.html'>デンキセキ</a>",
+        title: "<a href='../portfolio/denki.html'>デンキセキ</a>",
         description: "This is the description of article 18."
     },
     {
@@ -96,12 +96,12 @@ const articles = [
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/favcon.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/event.html#entry'>入門編ゲームジャム2022夏</a>",
+        title: "<a href='../portfolio/event.html#entry'>入門編ゲームジャム2022夏</a>",
         description: "This is the description of article 20."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0026.jpg",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/NPFM-0003.html'>忘れじの幻葬曲　～ Recollect Lost Fantasy</a>",
+        title: "<a href='../portfolio/NPFM-0003.html'>忘れじの幻葬曲　～ Recollect Lost Fantasy</a>",
         description: "This is the description of article 21."
     },
     {
@@ -116,62 +116,62 @@ const articles = [
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0042.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/nyax.html'>NyaX'22</a>",
+        title: "<a href='../portfolio/nyax.html'>NyaX'22</a>",
         description: "This is the description of article 24."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0022.jpg",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/rizulrimo.html'>Rizulrimo - The last tree</a>",
+        title: "<a href='../portfolio/rizulrimo.html'>Rizulrimo - The last tree</a>",
         description: "This is the description of article 25."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0023.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/obugyo.html'>OBUGYO</a>",
+        title: "<a href='../portfolio/obugyo.html'>OBUGYO</a>",
         description: "This is the description of article 26."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0152.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/covid.html'>COVID-19シューティング</a>",
+        title: "<a href='../portfolio/covid.html'>COVID-19シューティング</a>",
         description: "This is the description of article 27."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0043.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/cat.html'>CatKeynote</a>",
+        title: "<a href='../portfolio/cat.html'>CatKeynote</a>",
         description: "This is the description of article 28."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0021.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/shark.html'>サメサバイル</a>",
+        title: "<a href='../portfolio/shark.html'>サメサバイル</a>",
         description: "This is the description of article 29."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0024.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/rougoku.html'>蝋獄</a>",
+        title: "<a href='../portfolio/rougoku.html'>蝋獄</a>",
         description: "This is the description of article 30."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0165.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/spacecat.html'>宇宙ネコ</a>",
+        title: "<a href='../portfolio/spacecat.html'>宇宙ネコ</a>",
         description: "This is the description of article 31."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/favcon.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/event.html#spring2023'>2023春ジャム</a>",
+        title: "<a href='../portfolio/event.html#spring2023'>2023春ジャム</a>",
         description: "This is the description of article 32."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0192.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/syukatu.html'>転生しても就活生だった件</a>",
+        title: "<a href='../portfolio/syukatu.html'>転生しても就活生だった件</a>",
         description: "This is the description of article 33."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0020.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/sgf.html'>サウンドガンファイト</a>",
+        title: "<a href='../portfolio/sgf.html'>サウンドガンファイト</a>",
         description: "This is the description of article 34."
     },
     {
         image: "https://mixtuti.github.io/mixtuti/img/IMG_0201.png",
-        title: "<a href='https://mixtuti.github.io/mixtuti/portfolio/note.html'>AIを使ってケモノを生成する！</a>",
+        title: "<a href='../portfolio/note.html'>AIを使ってケモノを生成する！</a>",
         description: "This is the description of article 35."
     },
     {
