d5fde82faae2c903654d443e78bdead0432b408e
mixtuti桜餅 <88471532+mixtuti@users.noreply.github.com>
2023-06-06 08:40:27 +0900

    Add files via upload

--- Gmae/game.js
+++ Gmae/game.js
@@ -6,3 +6,11 @@ cookie.addEventListener("click", function() {
     count++;
     clickCount.textContent = count + " クッキー";
 });
+
+function enlargeCookie() {
+  const cookieImage = document.querySelector(".cookie-image");
+  cookieImage.classList.add("enlarged");
+  setTimeout(() => {
+    cookieImage.classList.remove("enlarged");
+  }, 100);
+}
